60dc212f8bee7a625dbbc7311ae44c8201aa8169
Jason <jconn99801@gmail.com>
2023-10-17 10:22:31 -0800

    fixed typo

--- server/app.py
+++ server/app.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///side.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
