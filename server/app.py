a5d3bb31c75828999ae6e020ed43a45dc93dde74
Jason <jconn99801@gmail.com>
2023-10-17 10:38:21 -0800

    imported models into app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
+from models.product import Product
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from models import db, Product
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
--- server/models.py
+++ server/models.py
@@ -6,3 +6,16 @@ class Product(db.Model):
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(255))
     price = db.Column(db.Float, nullable=False)
+
+    def __init__(self, name, description, quantity):
+        self.name = name
+        self.description = description
+        self.quantity = quantity
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'quantity': self.quantity
+        }
