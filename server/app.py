913dc1043bb967af24864f20e204348ddf1aea18
Jason <jconn99801@gmail.com>
2023-10-17 10:52:52 -0800

    small adjustments to importing models

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from models.product import Product
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import db, Product
+from server.models.models import db, Product
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
