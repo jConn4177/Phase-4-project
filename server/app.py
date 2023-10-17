26f85976ddf3c28a06e745fe7170c2ea29f7f695
Jason <jconn99801@gmail.com>
2023-10-17 10:53:09 -0800

    removed redundancies

--- server/app.py
+++ server/app.py
@@ -1,8 +1,7 @@
-from models.product import Product
+from server.models.product import Product
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from server.models.models import db, Product
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
