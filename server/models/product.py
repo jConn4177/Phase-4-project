4e7c205d1ca3e1b5e3996d16bb9227afc3b84084
Jason <jconn99801@gmail.com>
2023-10-17 10:53:39 -0800

    updated product.py

---
+++ server/models/product.py
@@ -0,0 +1,21 @@
+from server import db
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    price = db.Column(db.Float, nullable=False)
+
+    def __init__(self, name, description, quantity):
+        self.name = name
+        self.description = description
+        self.quantity = quantity
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'quantity': self.quantity
+        }
