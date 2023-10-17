a84c2163a7f5ba685a5b46cc42cded5b0c2e1037
Jason <jconn99801@gmail.com>
2023-10-17 10:51:41 -0800

    removed redundant init.py file

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,7 @@
-from server import routes
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from server import routes
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
