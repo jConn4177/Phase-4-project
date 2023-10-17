1a0c98c92e98a9b45d5edf9dab7d6c73fe5e83e8
Jason <jconn99801@gmail.com>
2023-10-17 10:38:03 -0800

    moved server import to top of init

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,7 @@
+from server import routes
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
-from server import routes
