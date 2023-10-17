02192e2d90468f718ca791f3b7d23aaa9163f6a4
Jason <jconn99801@gmail.com>
2023-10-17 14:48:02 -0800

    renamed category file

--- server/models/category.py
+++
@@ -1,7 +0,0 @@
-from server import db
-from server.models.serializers import SerializableMixin
-
-
-class Category(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(41), nullable=False, unique=True)
