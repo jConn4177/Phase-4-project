5fa4e6674ec319e7f07f552cba96e86cf83108fb
Rae Stanton <raestanton93@gmail.com>
2023-10-20 15:06:17 -0700

    fml

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,28 +1,40 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button"; // Add this for the button component from react-bootstrap
+import Button from "react-bootstrap/Button";
+import ordersImage from "../images/orders.png"; // Import your default image
 import "./ProductCard.css";
 
 function ProductCard({ product, onAddToCart, handleProductClick }) {
   return (
     <Card className="prod-cards h-card">
-      <Card.Img variant="top" src={product.image} className="prod-card-image" />
+      <Card.Img
+        variant="top"
+        src={product.image}
+        className="prod-card-image"
+        onError={(e) => {
+          e.target.src = ordersImage; // Set a default image when the original image fails to load
+        }}
+      />
 
       <Card.Body onClick={() => handleProductClick(product)}>
+        <Button
+          variant="primary"
+          className="form-button"
+          onClick={(e) => {
+            e.stopPropagation();
+            onAddToCart && onAddToCart();
+          }}
+        >
+          Add to Cart
+        </Button>
         <Card.Title>{product.name}</Card.Title>
         <Card.Text>{product.description}</Card.Text>
         <Card.Text>Price: ${product.price}</Card.Text>
         <Card.Text>Amount in Stock: {product.count}</Card.Text>
         <Card.Text>Categories: {product.category}</Card.Text>
-        {/* Add to Cart Button */}
-        <Button variant="primary" onClick={(e) => {
-          e.stopPropagation(); // This prevents the outer card click from being triggered
-          onAddToCart && onAddToCart();
-        }}>Add to Cart</Button>
       </Card.Body>
     </Card>
   );
 }
-
 
 export default ProductCard;
